4bab121318ef709b34b5b612e46a19be1ca1286a
Pedrooaj <pedroantonio5735@gmail.com>
2024-11-18 14:40:09 -0300

    update

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -10,7 +10,7 @@ const FormContainer = styled.div`
     align-items: center;
     flex-direction: column;
     width: 80%;
-    height: 45%;
+    height: auto;
     padding: 30px;
     border-radius: 20px;
     background-color: aqua;
