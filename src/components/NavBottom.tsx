6b3cf34a91f23173885ed616cd96c1b1ad779cb0
Pedrooaj <pedroantonio5735@gmail.com>
2024-11-19 12:15:27 -0300

    update

--- src/components/NavBottom.tsx
+++ src/components/NavBottom.tsx
@@ -9,7 +9,8 @@ const Nav = styled.div`
     display: flex;
     align-items: center;
     width: 100%;
-    height: 10dvh;
+    height: auto;
+    padding: 20px;
     position: absolute;
     bottom: 0;
     & ul {
